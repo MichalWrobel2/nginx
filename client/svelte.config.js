f53fe907de9257fedcaacf5214a072beba05eddf
Michał Wróbel <michalwrobel@diligentbits.com>
2023-03-28 23:34:17 +0200

    Removed sample sveltekit config

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -8,15 +8,7 @@ const config = {
 		prerender: {
 			entries: ['*']
 		},
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	}
 };
 
